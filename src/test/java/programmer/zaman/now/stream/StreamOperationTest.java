95eb16587f5b0dbd0625def675b5efa0eb6f9eb4
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-10 04:31:12 +0700

    stream operation

---
+++ src/test/java/programmer/zaman/now/stream/StreamOperationTest.java
@@ -0,0 +1,27 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOperationTest {
+    @Test
+    void  testStreamOperation(){
+
+        List<String> names = List.of("Hanif","Faiz","Hidayat");
+
+        Stream<String> streamNames = names.stream();
+        Stream<String> streamUpper = streamNames.map(name -> name.toUpperCase());
+
+        streamUpper.forEach(System.out::println);
+        System.out.println("---");
+        ///tidak merubah data lama / immutable
+        names.forEach(System.out::println);
+        
+    }
+
+    @Test
+    void testStreamPipeline() {
+    }
+}
