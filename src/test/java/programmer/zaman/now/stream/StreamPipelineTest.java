64327c440f2cbcfaece51a8a277e2451ab38f2c4
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-10 04:39:25 +0700

    stream pipeline

---
+++ src/test/java/programmer/zaman/now/stream/StreamPipelineTest.java
@@ -0,0 +1,37 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamPipelineTest {
+
+    @Test
+    void testCreateStreamPipeline() {
+
+        //JARANG DIGUNAKAN
+        List<String> list = List.of("Hanif","Faiz", "Hidayat");
+
+        Stream<String> stream = list.stream();
+
+        Stream<String> streamUpper = stream.map(name -> name.toUpperCase());
+
+        Stream<String> streamMr = streamUpper.map(upper -> "Mr. "+upper);
+
+        streamMr.forEach(System.out::println);
+
+    }
+
+    @Test
+    void testCreateStreamPipelineOk() {
+
+        List<String> list = List.of("Hanif","Faiz","Hidayat");
+
+        list.stream()
+                .map(name->name.toUpperCase())
+                .map(upper->"Mr."+upper)
+                .forEach(System.out::println);
+
+    }
+}
