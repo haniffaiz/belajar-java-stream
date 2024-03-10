b6fdd3acb048f5745deec8fb13be8ea1acbe2a03
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-10 21:02:18 +0700

    ordering operation

---
+++ src/test/java/programmer/zaman/now/stream/OrderingOperationTest.java
@@ -0,0 +1,25 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class OrderingOperationTest {
+
+    @Test
+    void testSorted() {
+        List.of("Hanif", "Faiz","Hidayat").stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void testSortedWithComparator() {
+        Comparator<String> reverseComparator = Comparator.reverseOrder();
+
+        List.of("Hanif", "Faiz","Hidayat").stream()
+                .sorted(reverseComparator)
+                .forEach(System.out::println);
+    }
+}
